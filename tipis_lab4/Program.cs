92ccdb7b1a8031644fbada101e2fea0c4fa1521b
wenddak1ng <49898464+wenddak1ng@users.noreply.github.com>
2019-04-23 15:07:46 +0700

    Add files via upload

---
+++ tipis_lab4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab_3 {
+  class MainClass {
+    static string selected;
+    static int shot;
+    static void Main(string[] args) {
+      Console.Title = "Лабораторная работа 4";
+      pulemet pul = new pulemet();
+      while (true) {
+        Console.WriteLine("Доступные действия: Выстрел / Ствол / Температура / Стоп / Остановить");
+        Console.Write("Для продолжения выберите действие: ");
+        selected = Console.ReadLine().ToLower();
+        if (selected == "выстрел") {
+          if (pul.Pause > DateTime.Now.Second) {
+            Console.WriteLine("Данное действие в данный момент невозможно. Время перезарядки {0} секунд", pul.Pause - DateTime.Now.Second);
+            return;
+          }
+          Console.Write("Введите количество выстрелов: ");
+          shot = int.Parse(Console.ReadLine());
+          Console.WriteLine("Текущая температура ствола {0}C", pul.Shot);
+          int i = 0;
+          if ((pul.Shot + (pul.nag1st * shot)) > pul.maxTst) {
+            i = shot - ((pul.maxTst - pul.Shot) / pul.nag1st);
+            shot = shot - i;
+          }
+          pul.Shot = shot; //++ temperature
+          pul.Pause = shot; //set tick
+          Console.WriteLine("Произведено {0} выстрелов", shot);
+          Console.WriteLine("Время паузы: {0} секунд", pul.Pause - DateTime.Now.Second); 
+          if (i > 0) {
+            Console.WriteLine("Не произведено {0} выстрелов из-за перегрева ствола", i);
+          }
+        }
+        else if (selected == "стоп") break;
+        else if (selected == "температура") pul.CheckTemp();
+        else if (selected == "ствол") pul.ChangeSt();
+        else if (selected == "остановить") {
+          Console.Write("Введите время паузы (в секундах): ");
+          pul.Pause = int.Parse(Console.ReadLine());
+        }
+      }
+    }
+  }
+}
---
+++ tipis_lab4/pulemet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab_3 {
+  public class pulemet {
+    public int V = 1, maxTst = 90, nag1st = 2, Vosts = 1;
+    const int Tokrsr = 23;
+    public int Stoptime;
+    public int tekTst = Tokrsr;
+    public int Shot {
+      get {
+        return tekTst;
+      } set {
+        tekTst = nag1st * value;
+      }
+    }
+    public void CheckTemp() => Console.WriteLine("Текущая температура ствола {0}C", tekTst);
+    public void ChangeSt() {
+      tekTst = Tokrsr;
+      Console.WriteLine("Ствол сменён. Текущая температура ствола {0}C", tekTst);
+    }
+    public int Pause {
+      get {
+        return Stoptime;
+      } set {
+        Stoptime = DateTime.Now.Second + (V * value);
+      }
+    }
+  }
+}
